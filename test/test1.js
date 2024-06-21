e1f9bc52cace39e4a49cac231ac722eceb30d41f
BMKa00 <carvajal_edo@hotmail.com>
2024-06-21 01:26:51 +0000

    ft:test1.js fixed, URL fixed

--- test/test1.js
+++ test/test1.js
@@ -14,21 +14,10 @@ async function test_case() {
 
     try {
         // Send driver to website
-        await driver.get("http://44.197.176.212");
-
-        // Find the body element and get its background color
-        let bodyElement = await driver.findElement(By.tagName('body'));
-        let backgroundColor = await bodyElement.getCssValue('background-color');
-
-        // Expected color
-        let expectedColor = 'rgb(0, 128, 0)'; // Expected color: green
-
-        // Check the result
-        if (backgroundColor !== expectedColor) {
-            throw new Error(`Background color mismatch. Expected: ${expectedColor}, Actual: ${backgroundColor}`);
-        } else {
-            console.log('Background color is as expected.');
+        await driver.get("http://3.93.40.136");
         }
+        
+        
     } catch (error) {
         console.log('Test Failed because:', error);
     } finally {
